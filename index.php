31a7a92a36fe0850abf22b0882e35d149c4aebe0
David Martín Pérez <davidmartinperez1@gmail.com>
2018-05-24 16:14:14 +0200

    add edge

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
     <head>
         <meta charset="utf-8">
         <title>POKEDEX</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="shortcut icon" type="image/png" href="https://vignette.wikia.nocookie.net/pokemonreloaded/images/4/4c/Pokeball.png/revision/latest?cb=20160823223254&path-prefix=es"/>
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
